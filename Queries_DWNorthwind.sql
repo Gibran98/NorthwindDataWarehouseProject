35d85c095c6eb215ef4c27e462f445c7cd94b445
BernardGU <bernardoc.gutierrezc@gmail.com>
2019-10-12 12:10:55 -0500

    Added queries 1, 2, 3 and 4 for DWNorthwind

--- Queries_DWNorthwind.sql
+++ Queries_DWNorthwind.sql
@@ -1,14 +1,27 @@
 /* Q1. Cual es el producto mas vendido en 1996? */
-
+select P.ProductName
+from DWNorthwind.dbo.DimProduct P
+where P.ProductID IN (
+	select top 1 with ties F.ProductID
+	from  DWNorthwind.dbo.FactSales F
+	where year(F.orderDate) = 1996
+	group by F.ProductID
+	order by count(F.ProductID) desc
+);
 
 /* Q2. Cual es el total de ventas (dinero) en 1996? */
-
+select sum(F.total)
+from DWNorthwind.dbo.FactSales F
+where year(F.orderDate) = 1996
 
 /* Q3. Cual es el total de ventas(dinero) en 1997? */
+select sum(F.total)
+from DWNorthwind.dbo.FactSales F
+where year(F.orderDate) = 1997
 
-
-/* Q4. Cual es el total de ventas en todos los años incluidos en la DB? */
-
+/* Q4. Cual es el total de ventas en todos los aï¿½os incluidos en la DB? */
+select sum(F.total)
+from DWNorthwind.dbo.FactSales F
 
 /* Q5. Cual region tuvo mas ventas (dinero) en 1997? */
 
@@ -16,4 +29,4 @@
 /* Q6. Para la respuesta de Q5 (region), cual es el estado (si es USA) o pais (region diferente a USA) que mas vendio en 1997? */
 
 
-/* Q7. Cual es el total de ventas en total (todos los años) organizado por Region, Estado y/o pais? */
+/* Q7. Cual es el total de ventas en total (todos los aï¿½os) organizado por Region, Estado y/o pais? */
